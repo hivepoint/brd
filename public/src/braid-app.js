38fe3e50c7b3decd87191aa63296a55dfd145f88
Preet <preet@hivepoint.com>
2017-05-31 11:39:33 -0700

    new shell.. work in progress

--- public/src/braid-app.js
+++ public/src/braid-app.js
@@ -1,5 +1,54 @@
 class BraidApp extends Polymer.Element {
   static get is() { return 'braid-app'; }
+
+  connectedCallback() {
+    super.connectedCallback();
+    window.$service = this.$.service;
+    window.addEventListener("resize", () => {
+      this.needsLayout();
+    });
+    Polymer.RenderStatus.beforeNextRender(this, () => {
+      this.refreshLayout();
+    });
+  }
+
+  onScroll() {
+    this.refreshScrollPosition();
+  }
+
+  needsLayout() {
+    if (this.layoutPending) {
+      return;
+    }
+    if (this.layoutDebouncing) {
+      this.layoutPending = true;
+      return;
+    }
+    this.refreshLayout();
+    this.layoutDebouncing = true;
+    setTimeout(() => {
+      this.layoutDebouncing = false;
+      if (this.layoutPending) {
+        this.refreshLayout();
+      }
+      this.layoutPending = false;
+    }, 600);
+  }
+
+  refreshLayout() {
+    this.$.topBuffer.style.height = this.$.toolbar.offsetHeight + "px";
+    this.refreshScrollPosition();
+  }
+
+  refreshScrollPosition() {
+    var offsetHeight = this.$.toolbarSubContent.offsetHeight || 0;
+    var ht = Math.max(offsetHeight - this.$.scrollPanel.scrollTop, 0);
+    if (this._prevBarHeight != ht) {
+      this.$.toolbarSub.style.height = ht + "px";
+      this.$.toolbarSub.style.opacity = ht / offsetHeight;
+      this._prevBarHeight = ht;
+    }
+  }
 }
 
 window.customElements.define(BraidApp.is, BraidApp);
